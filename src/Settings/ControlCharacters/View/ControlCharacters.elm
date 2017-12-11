cd0639532b706b13cadfab66f71ef2875d985689
Matthew King <mattking73@gmail.com>
2017-12-10 22:16:16 -0800

    Add ASCII / Hex preview to control characters

--- src/Settings/ControlCharacters/View/ControlCharacters.elm
+++ src/Settings/ControlCharacters/View/ControlCharacters.elm
@@ -1,5 +1,7 @@
 module Settings.ControlCharacters.View.ControlCharacters exposing (..)
 
+import Char
+import Hex as Hex exposing (toString)
 import Html exposing (Html, div, input, text, button, label)
 import Html.Attributes exposing (class, value)
 import Html.Events exposing (onClick, onInput)
@@ -16,27 +18,45 @@ view : Settings.Model -> Html Main.Msg
 view settings =
     SettingsLayout.view settings
         "Control Characters"
-        (div
-            [ class "form" ]
-            [ viewInput
-                settings.controlCharacters.startOfText
-                "Start of HL7"
-                (MsgForSettings << MsgForControlCharacters << UpdateStartOfText)
-            , viewInput
-                settings.controlCharacters.endOfText
-                "End of HL7"
-                (MsgForSettings << MsgForControlCharacters << UpdateEndOfText)
-            , viewInput
-                settings.controlCharacters.endOfLine
-                "End of Segment"
-                (MsgForSettings << MsgForControlCharacters << UpdateEndOfLine)
-            , button
-                [ class "btn btn-primary settings-save"
-                , onClick (MsgForSettings SaveSettings)
-                ]
-                [ text "Save" ]
+        (div []
+            [ viewHeading
+            , viewCharacters settings
             ]
         )
+
+
+viewHeading : Html Main.Msg
+viewHeading =
+    div []
+        [ label [ class "settings-label-control-character" ] [ text "Control Character" ]
+        , label [ class "settings-label-control-decimal" ] [ text "Decimal" ]
+        , label [ class "settings-label-control-ascii" ] [ text "ASCII" ]
+        , label [ class "settings-label-control-hex" ] [ text "Hex" ]
+        ]
+
+
+viewCharacters : Settings.Model -> Html Main.Msg
+viewCharacters settings =
+    div
+        [ class "form" ]
+        [ viewInput
+            settings.controlCharacters.startOfText
+            "Start of HL7"
+            (MsgForSettings << MsgForControlCharacters << UpdateStartOfText)
+        , viewInput
+            settings.controlCharacters.endOfText
+            "End of HL7"
+            (MsgForSettings << MsgForControlCharacters << UpdateEndOfText)
+        , viewInput
+            settings.controlCharacters.endOfLine
+            "End of Segment"
+            (MsgForSettings << MsgForControlCharacters << UpdateEndOfLine)
+        , button
+            [ class "btn btn-primary settings-save"
+            , onClick (MsgForSettings SaveSettings)
+            ]
+            [ text "Save" ]
+        ]
 
 
 viewInput : Int -> String -> (String -> Main.Msg) -> Html Main.Msg
@@ -47,7 +67,20 @@ viewInput controlCharacter labelText inputMsg =
         , input
             [ class "form-control"
             , onInput inputMsg
-            , value (toString controlCharacter)
+            , value (Basics.toString controlCharacter)
             ]
             []
+        , label [ class "settings-label-control-ascii" ] [ text (viewAsciiCharacter controlCharacter) ]
+        , label [ class "settings-label-control-hex" ] [ text (viewHexCode controlCharacter) ]
         ]
+
+
+viewAsciiCharacter : Int -> String
+viewAsciiCharacter character =
+    Char.fromCode (character)
+        |> Basics.toString
+
+
+viewHexCode : Int -> String
+viewHexCode character =
+    "0x" ++ (Hex.toString character)
