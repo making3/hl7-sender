72b6fbeae38303f92a29df188533f07cd2e3feaf
Matthew King <mattking73@gmail.com>
2017-12-22 10:12:15 -0800

    Minor refactor  to hl7 to match updateVersion

--- src/Home/Update.elm
+++ src/Home/Update.elm
@@ -9,16 +9,23 @@ import Home.Msg as Home exposing (..)
 update : Home.Msg -> Main.Model -> ( Main.Model, Cmd Main.Msg )
 update msg model =
     case msg of
-        ChangeHl7 newHl7 ->
-            ( { model | home = updateHl7 model.home newHl7 }, Cmd.none )
+        ChangeHl7 hl7 ->
+            ( updateHl7 model hl7, Cmd.none )
 
         Version version ->
             ( updateVersion model version, Cmd.none )
 
 
-updateHl7 : Home.Model -> String -> Home.Model
-updateHl7 home newHl7 =
-    { home | hl7 = newHl7 }
+updateHl7 : Main.Model -> String -> Main.Model
+updateHl7 model newHl7 =
+    let
+        home =
+            model.home
+
+        newHome =
+            { home | hl7 = newHl7 }
+    in
+        { model | home = newHome }
 
 
 updateVersion : Main.Model -> String -> Main.Model
