783b47f77d79a02f0e18e344cfc6d90f17539a4c
Matthew King <mattking73@gmail.com>
2017-12-22 08:49:45 -0800

    add basic about text

--- src/Home/View/About.elm
+++ src/Home/View/About.elm
@@ -1,13 +1,18 @@
 module Home.View.About exposing (..)
 
 import Msg exposing (..)
+import Model exposing (Model)
+import View.Layout.Modal as Layout exposing (view)
 import Html exposing (Html, div, label, text)
 
 
 view : Html Msg
 view =
-    div []
-        [ div []
-            [ label [] [ text "Test" ]
-            ]
-        ]
+    Layout.view
+        "About"
+        (div [] [ label [] [ text aboutText ] ])
+
+
+aboutText : String
+aboutText =
+    "This is text about the application"
