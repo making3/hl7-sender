e8bd6d76fc3a26c786c98fc64e5d0d42dadea31e
Matthew King <mattking73@gmail.com>
2018-01-22 19:17:06 -0800

    Add new connection to savedConnections list (append at front right now)

--- src/Settings/Update.elm
+++ src/Settings/Update.elm
@@ -2,9 +2,12 @@ module Settings.Update exposing (..)
 
 import Msg as Main exposing (..)
 import Model as Main exposing (..)
+import Route.Model as Root exposing (..)
+import Home.Route as Home exposing (..)
 import Settings.Msg as Settings exposing (..)
 import Settings.Commands exposing (..)
 import Settings.Model as Settings exposing (..)
+import Connection.Model as Connection exposing (Connection)
 import Settings.ControlCharacters.Update as ControlCharacters exposing (update)
 import Settings.ControlCharacters.Model as ControlCharacters exposing (encode)
 
@@ -51,4 +54,32 @@ update msg model =
                 ( { model | settings = newSettings }, Cmd.none )
 
         SaveConnection ->
-            ( model, Cmd.none )
+            ( addNewConnection model, Cmd.none )
+
+
+addNewConnection : Main.Model -> Main.Model
+addNewConnection model =
+    let
+        connection =
+            model.connection
+
+        newConnection =
+            { connection | savedConnections = appendConnectionToList model model.connection.savedConnections }
+    in
+        { model
+            | connection = newConnection
+            , route = Root.RouteHome Home.RouteHome
+        }
+
+
+appendConnectionToList : Main.Model -> List Connection -> List Connection
+appendConnectionToList model connections =
+    (getNewConnection model) :: connections
+
+
+getNewConnection : Main.Model -> Connection
+getNewConnection model =
+    { name = model.settings.newConnectionName
+    , destinationIp = model.connection.destinationIp
+    , destinationPort = model.connection.destinationPort
+    }
